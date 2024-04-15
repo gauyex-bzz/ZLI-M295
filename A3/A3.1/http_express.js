b2e2d9fce5bb62d564cc80ba39bc4046add000e6
gauyex-bzz <gauyex@bzz.ch>
2024-04-15 14:15:18 +0200

    UPDATED: http_express.js

---
+++ A3/A3.1/http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
