00b8a3c05cccc0fb02a3ae144435330c757499cf
gauyex-bzz <gauyex@bzz.ch>
2024-04-15 13:25:12 +0200

    UPDATED: promises.js

---
+++ A2.2/promises.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs');
+
+function leseDateiInhalt(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filepath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });
+    });
+}
+
+// Beispielaufruf
+leseDateiInhalt('textfile.txt')
+    .then(inhalt => {
+        console.log('Die Länge des Dateiinhalts beträgt:', inhalt.length);
+    })
+    .catch(err => {
+        console.error('Fehler beim Lesen der Datei:', err);
+    });
