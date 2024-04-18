3cb36d79a4a6f0ec25d84b4fe5a36f47dcdc60e8
gauyex-bzz <gauyex@bzz.ch>
2024-04-18 13:15:31 +0200

    UPDATED: basic_auth.js

---
+++ A7/A7.1/basic_auth.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+
+const app = express();
+
+// Public endpoint
+app.get('/public', (req, res) => {
+    res.send('Öffentlicher Inhalt.');
+});
+
+// Basic Auth middleware
+const authMiddleware = basicAuth({
+    users: { 'zli': 'zli1234' },
+    unauthorizedResponse: 'Zugriff verweigert: geben Sie Benutzername und Passwort ein.'
+});
+
+// Private endpoint
+app.get('/private', authMiddleware, (req, res) => {
+    res.send('Privater Inhalt, authentifiziert!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
